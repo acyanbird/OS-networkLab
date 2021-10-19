96cc5ac1165f1b340d7d93f454727f28733e5e39
acyanbird <2534930703@qq.com>
2021-10-19 13:51:05 +0100

    lab1

--- lab/lab1/main.c
+++ lab/lab1/main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+//
+// Created by lucia on 19/10/2021.
+//
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include "stdio.h"
+
+int main(){
+    int start, end, sum;
+    printf("Input the start number:");
+    scanf("%d", &start);
+    printf("Input the end number:");
+    scanf("%d", &end);
+
+    sum = primeSum(start, end);
+
+    printf("Sum is %d\n", sum);
+
 }
