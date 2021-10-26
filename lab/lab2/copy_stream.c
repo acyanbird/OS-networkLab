73b98cae7f1cd114e1cb0e9c0ccb227c272283aa
acyanbird <2534930703@qq.com>
2021-10-26 22:52:30 +0100

    a

--- lab/lab2/copy_stream.c
+++ lab/lab2/copy_stream.c
@@ -2,3 +2,19 @@
 // Created by lucia on 26/10/2021.
 //
 
+#include "stdio.h"
+#include "stdlib.h"
+
+#define SIZE 100    // len of str is 100
+
+int main(){
+    char name1[20], name2[20];
+    printf("Input file name 1:");
+    scanf("%s", name1);
+    FILE *fptr1 = fopen(name1,"r");
+
+    if(fptr1 == NULL) {
+        fprintf(stderr, "Error opening file\n");
+        exit(1);
+    }
+}
--- lab/lab2/ex1.c
+++ lab/lab2/ex1.c
@@ -2,3 +2,32 @@
 // Created by lucia on 26/10/2021.
 //
 
+// need to change, count the word at the end without space
+
+#include<stdio.h>
+#include<string.h>
+
+int count_words(char *str) {
+    // this function should return the number of words in str
+    // count the space num in str
+    int num = 0;
+    int len = 0;    // init counter
+    while (*str != '\0') {
+        if (*str != ' ') {
+            num++;
+        }
+        str++;
+    }
+    return num;
+}
+int main(){
+
+
+    char str[100];
+
+    printf("Enter a string:");
+    fgets(str,100,stdin);    // this function reads a line or at most 99 bytes from stdin file stream that represents the keyboard
+    printf("Number of words in the entered string is %d\n",count_words(str));
+
+    return 0;
+}
